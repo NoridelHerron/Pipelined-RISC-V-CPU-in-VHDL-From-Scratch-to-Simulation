4f645f9ad41859bce2e3a7458ff7a37e2f68dbac
Noridel Herron <noridel.herron@yahoo.com>
2025-05-22 03:37:55 -0500

    Added new source files

---
+++ src/IF_ID.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/17/2025 11:07:19 PM
+-- Design Name: 
+-- Module Name: IF_ID - behavior
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity IF_ID is
+--  Port ( );
+end IF_ID;
+
+architecture behavior of IF_ID is
+
+begin
+
+
+end behavior;
